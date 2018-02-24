b016bb2536868a6702568609d87060d5cca74b67
saranga <saranga@192.168.0.10>
2018-02-25 07:46:39 +0800

    commit wordcount project

---
+++ sparkexamples/src/main/scala/com/srsoft/sparkexamples/WordCount.scala
@@ -0,0 +1,39 @@
+package com.srsoft.sparkexamples
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object WordCount {
+  def main(args: Array[String]){
+    val conf = new SparkConf()
+    .setAppName("Word Count")
+    .setMaster("local")
+    
+    val sc = new SparkContext(conf)
+    
+      if(args.length < 2) {
+      println("Missing Arguments !!!")
+    }
+    
+    // Loading text fileFile object 
+    val textFile = sc.textFile(args(0))
+
+  
+    
+    val words = textFile.flatMap(line => line.split(" "))
+    
+    val counts = words.map(word => (word,1))
+    
+    val wordcount = counts.reduceByKey(_ + _)
+    
+    val wordcount_sorted = wordcount.sortByKey()
+    
+    wordcount_sorted.foreach(println)
+    
+   wordcount_sorted.saveAsTextFile(args(1))
+    
+    
+  }
+  
+  
+  
+}
